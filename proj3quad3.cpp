71b505a6c3bac971af7c755f4977a91913f69612
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-05-28 12:18:36 +1200

    Changed line_error calculations

--- proj3quad3.cpp
+++ proj3quad3.cpp
@@ -68,9 +68,9 @@ int Robot::FollowLine(){
 		v_left = v_left_go - dv;
 		v_right = v_right_go + dv;
 		if(v_left > 65) {
-		v_left = 65;
-		} else if(v_right < 30){
-		v_right = 30;
+		v_left = 64;
+		} else if(v_right < 31){
+		v_right = 31;
 		}
 		
 		printf(" %nline error: %.1f dv: %d",line_error,dv);
@@ -78,8 +78,8 @@ int Robot::FollowLine(){
 		
 	} else {
 			printf(" Line missing");
-			v_left = 65;
-			v_right = 30;
+			v_left = 64;
+			v_right = 31;
 			SetMotors();
 			
 	}
