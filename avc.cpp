c60bd4b5cef1e6b46467b5bbdc20671bd94ef901
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-05-31 13:21:00 +1200

    chang spd and kd

--- avc.cpp
+++ avc.cpp
@@ -14,10 +14,10 @@ class Robot{
     int quadrant = 1;
     const int cam_width = 320;
     const int cam_height = 240;
-    const int v_left_go = 53;
-    const int v_right_go = 44;
-    double kp = 0.001;
-    double kd = 0.003;
+    const int v_left_go = 52;
+    const int v_right_go = 45;
+    double kp = 0.0001;
+    double kd = 0.0003;
     double err;
     int line_present = 1;
     int prev_error;
