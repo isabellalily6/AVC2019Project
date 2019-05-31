230a86333d77b288e15844e5458b60ae3e314645
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-05-31 19:49:42 +1200

    adj sleep

--- quad3.cpp
+++ quad3.cpp
@@ -216,7 +216,7 @@ int Robot::MeasureLine(){ //only coded for quad 2 rn
 			if(line3 > 230) { //crossroad sensor
 				turnLeftBool =1;
 				printf("\nrobot is at a cross roads");
-				sleep1(300);
+				sleep1(270);
 				return 0;
 			} else if(line3 < 10) {
 				printf("\n\n\n\n Dead End\n\n\n\n");
