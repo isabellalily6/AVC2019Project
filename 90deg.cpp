8a10d8774b7026cb95fc599c5bd2c46781519452
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-05-31 15:25:33 +1200

    90deg changin 180 slp (SPD IS PERFECT FOR ON THE SPOT)

--- 90deg.cpp
+++ 90deg.cpp
@@ -81,7 +81,7 @@ void Robot::fullTurn() {
 		v_left = 41;
 		v_right = 45;
 		SetMotors();
-		sleep1(3400);
+		sleep1(3000);
 		printf("turn around over");
 		v_left = v_left_go;
 		v_right =v_right_go;
