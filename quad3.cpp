6178a91f16ce0293de153f94c1a178638fe79056
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-06-01 13:33:43 +1200

    changing how xroads work

--- quad3.cpp
+++ quad3.cpp
@@ -129,6 +129,12 @@ int Robot::MeasureLine(){ //only coded for quad 2 rn
 			if(lineCount < 50 ) { //0 might be too harsh for this - needs testing
 					reverseBool = 1; //if the line is not present reverse
 					return 0;
+			} else if (lineCount > 240){
+			printf("\n\n\n Robot is at a cross road\n\n\n")	;
+			v_left = v_left_go - 2;
+			v_right = v_right_go + 2;
+			SetMotors();
+			sleep1(60);
 			}
 			
 				
