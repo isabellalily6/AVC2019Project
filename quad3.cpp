863811ec17d2e00779b33ccd394ef25ec715b571
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-06-02 14:25:06 +1200

    reading vals

--- quad3.cpp
+++ quad3.cpp
@@ -19,8 +19,8 @@ class Robot{
     int quadrant = 2;
     const int cam_width = 320;
     const int cam_height = 240;
-    const int v_left_go = 51;
-    const int v_right_go = 45;
+    const int v_left_go = 48;
+    const int v_right_go = 48;
     double kp = 0.0005;
     double kd = 0.0009;
     double err;
@@ -188,7 +188,7 @@ int Robot::MeasureLine(){ //only coded for quad 2 rn
 				v_right = v_right_go - 5;
 				SetMotors();
 				sleep1(1400);
-			} else if (lineCount >= 240 && lineCount - prevLineCount < 60){
+			} else if (lineCount >= 270 && lineCount - prevLineCount < 60){
 			printf("\n\n\n Robot is at a cross road\n\n\n")	;
 			v_left = v_left_go;
 			v_right = v_right_go;
