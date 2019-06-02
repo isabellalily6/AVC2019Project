cddeff39d798c4d31293b723c2885aaaf334ff85
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-06-02 14:06:27 +1200

    messing with spd

--- quad3.cpp
+++ quad3.cpp
@@ -21,7 +21,7 @@ class Robot{
     const int cam_height = 240;
     const int v_left_go = 52;
     const int v_right_go = 44;
-    double kp = 0.0003;
+    double kp = 0.0008;
     double kd = 0.0007;
     double err;
     double prevErr = 0;
