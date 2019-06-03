c1de9ad92f6b38c97847be0255ce9d976c5edecb
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-06-03 15:14:30 +1200

    changing the 90deg to make it better (me thinks)

--- avc.cpp
+++ avc.cpp
@@ -170,7 +170,7 @@ int Robot::MeasureLine(){ //only coded for quad 2 rn
 					
 					reverseBool = 1; //if the line is not present reverse
 					return 0;
-			} else if ((lineCount >= 230 && (err < 1 || err > 5.25) && (lineCount - prevLineCount) < 50 && xroadBool == 1 && corner == 0) || lineCount > 270){
+			} else if ((lineCount >= 230 && (err < 1 || err > 5.25) && (lineCount - prevLineCount) < 50 && xroadBool == 1 && corner == 0 && (err - prevErr < 1) || lineCount > 270){
 			printf("\n------------------------------------------------------------------------------------\n\n\n\n Robot is at a cross road\n\n\n\n\n")	;
 			v_left = v_left_go;
 			v_right = v_right_go;
