3352cd245a813447e95af99a4e47c23836620c44
Callumm242 <30997271+Callumm242@users.noreply.github.com>
2019-06-04 10:58:01 +1200

    adding detection for top of t

--- avc.cpp
+++ avc.cpp
@@ -149,7 +149,7 @@ int Robot::MeasureLine(){ //only coded for quad 2 rn
 			 */
 			for(int countCol = 0; countCol < 320; countCol++){
 			
-			totwhite = get_pixel(240/2, countCol,3); //for err line 
+			totwhite = get_pixel(1, countCol,3); //for err line 
 			//printf("\n\n\n\ntotwhite: %.5f",totwhite);
 			
 			
